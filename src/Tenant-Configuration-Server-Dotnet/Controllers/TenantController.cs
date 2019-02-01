02519262b4e4d40cacb295ba28af0002b757c6dc
mp <mp@example.com>
2019-02-01 10:31:38 +0100

    Tenant Controller + Test GRUD html

--- src/Tenant-Configuration-Server-Dotnet/Controllers/TenantController.cs
+++ src/Tenant-Configuration-Server-Dotnet/Controllers/TenantController.cs
@@ -1,50 +1,80 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Tenant_Configuration_Server_Dotnet.Models;
 
-namespace Tenant_Configuration_Server_Dotnet.Controllers
-{
-    [Route("api/tenant")]
+namespace Tenant_Configuration_Server_Dotnet.Controllers {
+    [Route ("api/tenant")]
     [ApiController]
-    public class TodoController : ControllerBase
-    {
+    public class TenantController : ControllerBase {
         private readonly TenantContext _context;
 
-        public TodoController(TenantContext context)
-        {
+        public TenantController (TenantContext context) {
             _context = context;
 
-            if (_context.Tenants.Count() == 0)
-            {
+            if (_context.Tenants.Count () == 0) {
                 // Create a new Tenant if collection is empty,
-                // which means you can't delete all TodoItems.
-                _context.Tenants.Add(new Tenant { Name = "Tenant1" });
-                _context.SaveChanges();
+                // which means you can't delete all Tenants.
+                _context.Tenants.Add (new Tenant { Name = "Tenant1" });
+                _context.SaveChanges ();
             }
         }
 
         // GET: api/Tenant
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Tenant>>> GetTenats()
-        {
-            return await _context.Tenants.ToListAsync();
+        public async Task<ActionResult<IEnumerable<Tenant>>> GetTenats () {
+            return await _context.Tenants.ToListAsync ();
         }
 
         // GET: api/Tenant/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Tenant>> GetTodoItem(long id)
-        {
-            var todoItem = await _context.Tenants.FindAsync(id);
+        [HttpGet ("{id}")]
+        public async Task<ActionResult<Tenant>> GetTenant (long id) {
+            var tenant = await _context.Tenants.FindAsync (id);
 
-            if (todoItem == null)
-            {
-                return NotFound();
+            if (tenant == null) {
+                return NotFound ();
             }
 
-            return todoItem;
+            return tenant;
+        }
+
+        // POST: api/Tenant
+        [HttpPost]
+        public async Task<ActionResult<Tenant>> PostTenant (Tenant tenant) {
+            _context.Tenants.Add (tenant);
+            await _context.SaveChangesAsync ();
+
+            return CreatedAtAction (nameof (GetTenant), new { id = tenant.Id }, tenant);
+        }
+
+        // PUT: api/Tenant/5
+        [HttpPut ("{id}")]
+        public async Task<IActionResult> PutTenant (long id, Tenant tenant) {
+            if (id != tenant.Id) {
+                return BadRequest ();
+            }
+
+            _context.Entry (tenant).State = EntityState.Modified;
+            await _context.SaveChangesAsync ();
+
+            return NoContent ();
+        }
+
+        // DELETE: api/Tenant/5
+        [HttpDelete ("{id}")]
+        public async Task<IActionResult> DeleteTenant (long id) {
+            var tenant = await _context.Tenants.FindAsync (id);
+
+            if (tenant == null) {
+                return NotFound ();
+            }
+
+            _context.Tenants.Remove (tenant);
+            await _context.SaveChangesAsync ();
+
+            return NoContent ();
         }
     }
 }
